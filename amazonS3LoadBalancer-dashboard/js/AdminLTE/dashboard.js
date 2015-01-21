8978f3b90c0b00ba81e6b0b8daea188d1dddb0fa
Ramya <ramyakrishna5711@gmail.com>
2015-01-20 23:18:31 -0800

    modified few UI elements

--- amazonS3LoadBalancer-dashboard/js/AdminLTE/dashboard.js
+++ amazonS3LoadBalancer-dashboard/js/AdminLTE/dashboard.js
@@ -1,21 +1,6 @@
 
 $(function() {
 
-    //jvectormap data
-    var visitorsData = {
-        "US": 398, //USA
-        "SA": 400, //Saudi Arabia
-        "CA": 1000, //Canada
-        "DE": 500, //Germany
-        "FR": 760, //France
-        "CN": 300, //China
-        "AU": 700, //Australia
-        "BR": 600, //Brazil
-        "IN": 800, //India
-        "GB": 320, //Great Britain
-        "RU": 3000 //Russia
-    };
-    
     /*var countries = [
         {name: 'Singapore', coords: [1.30, 103.80], status: 'old'},
         {name: 'Tokyo', coords: [35.68, 139.69], status: 'new'},
@@ -44,7 +29,7 @@ $(function() {
                         });
                     } 
                 });
-                console.log(arr);
+                //console.log(arr);
                 return arr;
         })();
     
