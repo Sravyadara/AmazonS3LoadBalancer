387a21a831701b95dcb42fba80d9b419c0f8b60a
Ramya <ramyakrishna5711@gmail.com>
2015-01-20 20:08:16 -0800

    modified the map locations plugin

--- amazonS3LoadBalancer-dashboard/js/AdminLTE/dashboard.js
+++ amazonS3LoadBalancer-dashboard/js/AdminLTE/dashboard.js
@@ -16,7 +16,7 @@ $(function() {
         "RU": 3000 //Russia
     };
     
-    var plants = [
+    /*var countries = [
         {name: 'Singapore', coords: [1.30, 103.80], status: 'old'},
         {name: 'Tokyo', coords: [35.68, 139.69], status: 'new'},
         {name: 'Oregon', coords: [44.00,-120.50], status: 'new'},
@@ -25,8 +25,29 @@ $(function() {
         {name: 'Frankfurt', coords: [50.11,8.68], status: 'new'},
         {name: 'Sydney', coords: [-33.86,151.20], status: 'new'},
         {name: 'Sao Paulo', coords: [23.55,46.63], status: 'new'}
-    ];
-
+    ];*/
+    
+    var countries = (function() {
+                var arr = []
+                $.ajax({
+                    url: "http://localhost:9000/compute/AmazonS3/DisplayRegions",
+                    async: false,
+                    success: function(result) {
+                        $.each(result, function(k,v) {
+                            $.each(v, function(k1,v1) {
+                                if(k1 == "coords") {
+                                    v1 = v1.split(",");
+                                    result[k][k1] = v1;
+                                }
+                            arr.push(v);
+                          });
+                        });
+                    } 
+                });
+                console.log(arr);
+                return arr;
+        })();
+    
     $('#world-map').vectorMap({
     map: 'world_mill_en',
     backgroundColor: "transparent",
@@ -39,7 +60,7 @@ $(function() {
             r: 6
             }
         }, */
-    markers: plants.map(function(h){ 
+    markers: countries.map(function(h){ 
                             if(h.status == "old") { 
                                 return {name: h.name, latLng: h.coords, style: {fill: 'red'}} 
                             }
@@ -50,7 +71,7 @@ $(function() {
     labels: {
         markers: {
           render: function(index){
-            return plants[index].name;
+            return countries[index].name;
           }
          
         }
