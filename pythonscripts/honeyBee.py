23c76af6e28021fb88e8bf2809f8ebad22d33a8b
Ramya <ramyakrishna5711@gmail.com>
2015-01-31 20:22:25 -0800

    comminting new file

--- pythonscripts/honeyBee.py
+++ pythonscripts/honeyBee.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python
 from boto.s3.connection import S3Connection
 from boto.s3.key import Key
-import sys,os,ast
+import sys,os,time
 
 print "Executing HoneyBee algorithm and uploading photos"
 conn = S3Connection()
@@ -12,7 +12,7 @@ photosLocation = userHome + "/photos/"
 bucketsSizes = {}
 requestNumber = sys.argv[1]
 argument = sys.argv[2]
-argument = (argument.rstrip(']')).lstrip('[')
+argument = argument.translate(None, ']["')
 filesList = argument.split(",")
 
 for bucket in allBuckets:
@@ -23,14 +23,15 @@ for bucket in allBuckets:
     bucketsSizes[bucket.name] = size
 
 for i in range(0, int(requestNumber)):
+    print "Processing request : %d" %(i)
     for value in filesList:
         fileName = photosLocation + value +".bmp"
         bucketToUpload = min(bucketsSizes, key = bucketsSizes.get)
-        print "Uploading file: %s to bucket : %s" %(fileName, bucketToUpload)
         k = Key(conn.get_bucket(bucketToUpload))
-        k.key = os.path.basename(fileName)
+        uniqueKey = value + "_" + str(int(time.time())) + ".bmp"
+        k.key = uniqueKey
+        print "\nUploading file: %s to bucket : %s" %(fileName, bucketToUpload)
         k.set_contents_from_filename(fileName)
         fileSize = os.path.getsize(fileName)
-        print "\nUpdated bucketsSizes\n"
+        print "\nRefreshing bucket sizes cahce\n"
         bucketsSizes[bucketToUpload] += fileSize
-        print bucketsSizes
